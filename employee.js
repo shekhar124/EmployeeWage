49debade7512093e07a4c55bda9e5e308a224d75
rajashekhar <rajashekharpasikanti@gmail.com>
2022-01-30 11:21:04 +0530

    daily working hours

--- employee.js
+++ employee.js
@@ -1,24 +1,26 @@
-// Constants
-let EMP_RATE_PER_HOUR = 20;
-let IS_PART_TIME = 1;
-let IS_FULL_TIME = 2;
+const IS_ABSENT=0;
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HRS=4;
+const FULL_TIME_HRS=8;
+const EMP_WAGE_PER_HRS=20
 
-// Variables
-let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = 0, empWage = 0;
 
-switch(empCheck){
+function getWorkingHours(empCheck){
+switch(empCheck) {
+    
     case IS_PART_TIME:
-        empHrs = 4;
-        break
+        return PART_TIME_HRS;
+
     case IS_FULL_TIME:
-        empHrs = 8;
-        break
+        return FULL_TIME_HRS;
     default:
-        empHrs = 0;
+        return 0;
+   }    
 }
 
-empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log("Employee Wage = "+empWage);
-
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+empHrs = getWorkingHours(empCheck);
+let empWage=empHrs* EMP_WAGE_PER_HRS;
+console.log("Employee Wage is :" + empWage);
