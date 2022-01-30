d84cce68b48733e30d24f651c846fb93260d693c
rajashekhar <rajashekharpasikanti@gmail.com>
2022-01-30 11:25:39 +0530

    calculate montly wage

--- employee.js
+++ employee.js
@@ -3,8 +3,8 @@ const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HRS=4;
 const FULL_TIME_HRS=8;
-const EMP_WAGE_PER_HRS=20
-
+const EMP_WAGE_PER_HRS=20;
+const NUM_OF_WORKING_DAYS= 20;
 
 function getWorkingHours(empCheck){
 switch(empCheck) {
@@ -18,9 +18,13 @@ switch(empCheck) {
         return 0;
    }    
 }
-
 let empHrs=0;
+for (let day=0; day<NUM_OF_WORKING_DAYS; day++) {
 let empCheck=Math.floor(Math.random()*10)%3;
-empHrs = getWorkingHours(empCheck);
+empHrs += getWorkingHours(empCheck);
+
+}
+
 let empWage=empHrs* EMP_WAGE_PER_HRS;
-console.log("Employee Wage is :" + empWage);
+
+console.log("Total Hrs: "+ empHrs +" Employee Wage is :" + empWage);
