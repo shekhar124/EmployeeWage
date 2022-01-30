9fb4604de329a8db4688720a36e18e4a1be44e45
rajashekhar <rajashekharpasikanti@gmail.com>
2022-01-30 11:30:03 +0530

    calculate wage till no. of working days

--- employee.js
+++ employee.js
@@ -4,7 +4,8 @@ const IS_FULL_TIME=2;
 const PART_TIME_HRS=4;
 const FULL_TIME_HRS=8;
 const EMP_WAGE_PER_HRS=20;
-const NUM_OF_WORKING_DAYS= 20;
+const NUM_OF_WORKING_DAYS= 10;
+const MAX_HRS_IN_MONTH=100;
 
 function getWorkingHours(empCheck){
 switch(empCheck) {
@@ -18,13 +19,12 @@ switch(empCheck) {
         return 0;
    }    
 }
-let empHrs=0;
-for (let day=0; day<NUM_OF_WORKING_DAYS; day++) {
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) { 
+totalWorkingDays++;
 let empCheck=Math.floor(Math.random()*10)%3;
-empHrs += getWorkingHours(empCheck);
-
+totalEmpHrs += getWorkingHours(empCheck);
 }
-
-let empWage=empHrs* EMP_WAGE_PER_HRS;
-
-console.log("Total Hrs: "+ empHrs +" Employee Wage is :" + empWage);
+let empWage=totalEmpHrs* EMP_WAGE_PER_HRS;
+console.log("Total Days: "+ totalWorkingDays +" Total Hrs: "+ totalEmpHrs +" Employee Wage is: " + empWage);
