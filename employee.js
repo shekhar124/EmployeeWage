183e8f3763f68532db9987ddf82d0fba6e46b6da
rajashekhar <rajashekharpasikanti@gmail.com>
2022-01-30 10:53:19 +0530

    Attendance checking

---
+++ employee.js
@@ -0,0 +1,9 @@
+//uc1
+let isPresent = 1;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == 1)
+    console.log("Employee is Present");
+else
+    console.log("Employee is Absent");
+
+
